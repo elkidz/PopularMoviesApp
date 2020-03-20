599fc15934733f1b3d6dc5fd86a5caaf2e611878
Daniel Ribeiro <elkidz@users.noreply.github.com>
2020-03-20 14:43:25 +0000

    +-> Create constructor, getters and setters

--- app/src/main/java/com/example/popularmovies/data/database/Movie.java
+++ app/src/main/java/com/example/popularmovies/data/database/Movie.java
@@ -1,10 +1,75 @@
 package com.example.popularmovies.data.database;
 
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 import java.io.Serializable;
 
 @Entity(tableName = "movie")
 public class Movie implements Serializable {
-    
+
+    @PrimaryKey
+    private int id;
+    private String title;
+    private String releaseDate;
+    private String poster;
+    private double voteAverage;
+    private String overview;
+
+    public Movie(int id, String title, String releaseDate, String poster, double voteAverage, String overview) {
+        this.id = id;
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.poster = poster;
+        this.voteAverage = voteAverage;
+        this.overview = overview;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
 }
