83fdc15c81c1227e3731b71aa8795a710b3b1f56
Daniel Ribeiro <elkidz@users.noreply.github.com>
2020-03-20 15:27:45 +0000

    +-> AppExecutors

---
+++ app/src/main/java/com/example/popularmovies/utils/AppExecutors.java
@@ -0,0 +1,6 @@
+package com.example.popularmovies.utils;
+
+//TODO https://github.com/android/architecture-components-samples/blob/master/PersistenceMigrationsSample/app/src/main/java/com/example/android/persistence/migrations/AppExecutors.java
+public class AppExecutors {
+    
+}
