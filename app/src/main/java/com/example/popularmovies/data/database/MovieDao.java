6df4341680a05eb4d0a2e5db6072e9b333c3597f
Daniel Ribeiro <elkidz@users.noreply.github.com>
2020-03-20 16:14:22 +0000

    *-> Fixing mistakes about database

--- app/src/main/java/com/example/popularmovies/data/database/MovieDao.java
+++ app/src/main/java/com/example/popularmovies/data/database/MovieDao.java
@@ -1,4 +1,25 @@
 package com.example.popularmovies.data.database;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface MovieDao {
+
+    @Query("SELECT * FROM movie")
+    LiveData<List<Movie>> getMoviesFavorite();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Movie movie);
+
+    @Query("DELETE FROM movie WHERE id = :movieId")
+    void delete(int movieId);
+
+    @Query("SELECT EXISTS(SELECT 1 FROM movie WHERE id = :movieId LIMIT 1)")
+    LiveData<Boolean> isFavorite(int movieId);
 }
